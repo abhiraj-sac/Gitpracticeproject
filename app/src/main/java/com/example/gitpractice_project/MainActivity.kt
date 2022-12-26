50b32e8eeb3a41537846ed8eb882b36cb615dcf2
ASUS <abhirajsachan706@gmail.com>
2022-12-26 11:07:38 +0530

    bug fixed 4

--- app/src/main/java/com/example/gitpractice_project/MainActivity.kt
+++ app/src/main/java/com/example/gitpractice_project/MainActivity.kt
@@ -7,8 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        print("this is newer")
-        today is fucked
+        print("hello ")
     }
 }
